fdd82331c9f0d7803742da9b638806731999f4d4
rkartzman <remykartzman@gmail.com>
2015-02-18 13:08:02 -0500

    Add routes for responding to mantras with AJAX

--- app/controllers/responses.rb
+++ app/controllers/responses.rb
@@ -1,1 +1,30 @@
-responses.rb
+get '/response/all' do 
+
+end 
+
+get '/response/new' do
+
+end 
+
+post '/response/new' do
+	response = Response.create(params[:response])
+	response.commenter_id = current_user.id 
+
+	if response.save
+		redirect "mantra/#{mantra.id}"
+	else 
+		redirect 'mantra/all'
+	end 
+end 
+
+post '/response.json' do
+	content_type :json
+	response = Response.new(params[:response])
+	response.commenter_id = current_user.id 
+
+	if response.save
+		response.to_json
+	else 
+		redirect '/mantra/all'
+	end 
+end 
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+ 	$('form#new-response').on('submit', function(event){
+ 		event.preventDefault();
+ 		var $form = $(event.target);
+ 		var $data = $(this).data();
+ 		$.ajax({
+ 			type: "post",
+ 			url: $data.url,
+ 			data: $form.serialize(),
+ 			success: function(response){
+ 				var selector = $('#responde')
+ 				$(selector).append(response.content)
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+ 			},
+ 			error: function(){
+ 				console.log("Stuff Ain't Right");
+ 				// add error handling here
+ 			}
+ 		})
+ 	})
 });
