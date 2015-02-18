2824f2aec3ccf1ebe5e0e006b02efc9d0494f37f
rkartzman <remykartzman@gmail.com>
2015-02-18 10:20:40 -0500

    Add user login and signup

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,37 @@
 get '/' do
-  erb :welcome
+  redirect '/login'
 end
 
 get '/login' do
-  
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect "/user/#{user.id}"
+  else
+    redirect '/login'
+  end
 end
 
 get '/signup' do
-  
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect '/login'
+    ##### Change to redirect to homepage
+  else
+    redirect '/signup'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/login'
 end
