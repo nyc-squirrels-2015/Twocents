60ff76a2682ebf9360ad51fbd21eff21b51f86aa
rkartzman <remykartzman@gmail.com>
2015-02-18 16:38:30 -0500

    Add AJAX for upquotes

--- public/js/application.js
+++ public/js/application.js
@@ -13,11 +13,27 @@ $(document).ready(function() {
 
  			},
  			error: function(){
- 				console.log("Stuff Ain't Right");
+ 				console.log("Stuff Ain't Right")
+ 				failResponse.show("Stuff Ain't Right")
  				// add error handling here
  			}
  		})
  	})
+
+ 	$('.vote-button').click(function(event){
+ 		event.preventDefault();
+
+ 		var url = $(this).attr("href");
+ 		$.ajax({
+ 			type: "get",
+ 			url: url
+ 		}).done(function(response){
+ 			var selector = "#" + response.mantra_id
+ 			$(selector).text(response.upquotes);
+
+ 		})
+ 	})
+
 
  	$("#mantra-btn").on('click', function(){
  		$('.user-mantras').toggle("user-mantras");
@@ -25,5 +41,26 @@ $(document).ready(function() {
  	$('#respond-btn').on('click', function(){
  		$('.response-box').toggle("response-box");
  	})
+
+
+ 	// var failResponse = (function(){
+ 	// 	var div = '.error';
+ 	// 	showError = function(message){
+ 	// 		$(div).text(message);
+ 	// 		$(div).toggle(true)
+ 	// 	};
+
+ 	// 	var hideError = function(){
+ 	// 		$(div).toggle(false)
+ 	// 	};
+
+ 	// 	return {
+ 	// 		showError: showError
+ 	// 		hide: hideError
+ 	// 	};
+ 	// })();
+
+ 	
+
 
 });
